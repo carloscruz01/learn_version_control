578561cc65722dbaf7af5de065942c1642d98804
carloscruz01 <contato.carloscruz29@gmail.com>
2022-11-26 19:55:41 -0300

    test.

--- hello_world.R
+++ hello_world.R
@@ -1,2 +1,6 @@
+library(usethis)
 print('hello world, this is my first code in version control')
 print("I am carloscruz01 and I'm a padawan teen." )
+usethis::use_git_config(user.name = "carloscruz01",
+                        user.email = "contato.carloscruz29@gmail.com")
+usethis::use_git()
